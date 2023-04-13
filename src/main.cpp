c2002e45be7daa95fb17ec41a99634a05721886a
Michael Kan <michael.kan.mail@gmail.com>
2023-04-13 20:36:19 +0200

    add proto read and write to files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bitset>
 #include <vector>
+#include <string>
+#include <fstream>
 
 using byte = unsigned char;
 const byte MAX_BYTE = 0xFF;
@@ -9,7 +11,7 @@ const int BITS_IN_BYTE = 8;
 
 
 
-void PrototypeConvertCodesToBytes(const std::vector<size_t>& codes, std::vector<byte>& bytes) { 
+void ProtoConvertCodesToBytes(const std::vector<size_t>& codes, std::vector<byte>& bytes) { 
     size_t offset{};
     size_t currentByteIndex{};
     if (bytes.size() == 0)
@@ -36,7 +38,7 @@ void PrototypeConvertCodesToBytes(const std::vector<size_t>& codes, std::vector<
     }
 }
 
-void PrototypeConverBytesToCodes(const std::vector<byte> &bytes, std::vector<size_t> &codes)
+void ProtoConverBytesToCodes(const std::vector<byte> &bytes, std::vector<size_t> &codes)
 {
     if (codes.size() == 0)
         codes.push_back(0);
@@ -68,41 +70,68 @@ void PrototypeConverBytesToCodes(const std::vector<byte> &bytes, std::vector<siz
             if (offset > 0)
                 i--;
         }
-        // if (codeSize > BITS_IN_BYTE) {
-        //     ssize_t shift{static_cast<ssize_t>(byteoffset - offset)};
-        //     codes[currentCodeIndex] += shift > 0 ? (size_t)bytes[i] << shift : (size_t)bytes[i] >> +shift;
-        //     byteoffset += BITS_IN_BYTE;
-        //     codeSize -= BITS_IN_BYTE;
-        // }
-        // else {
-        //     codes[currentCodeIndex] += (size_t)bytes[i] & (0xFF >> +codeSize) << (byteoffset - offset);
-        //     currentCodeIndex++;
-        //     offset = BITS_IN_BYTE + codeSize;
-        //     codes.push_back(0);
-        //     i--;
-        //     byteoffset = 0;
-        //     codeSize = CODE_SIZE;
-        // }
     }
 }
 
+// reads codes that was writen by ProtoWriteToFileCodes file
+void ProtoReadFromFileConvertToCodes(const std::string file, std::vector<size_t> &codes)
+{
+    std::ifstream in(file, std::ios::in | std::ios::binary); // or use full path "C:/example.txt"
+    if (in.is_open())
+    {
+        byte buf[100];
+        in.read(reinterpret_cast<char*>(buf), sizeof(buf));
+        std::vector<byte> bytes{buf, buf + sizeof(buf) / sizeof(buf[0])};
+        size_t eof_code = 0b1111111111;
+        ProtoConverBytesToCodes(bytes, codes);
+        // for (size_t i = 0; i < codes.size(); i++)
+        // {
+        //     if (codes[i] == eof_code) {
+        //         codes.resize(i + 1);
+        //         break;
+        //     }
+        // }
+    }
+    else
+    {
+        std::cout << "Unable to open file.\n";
+    }
+}
+
+// write codes 
+void ProtoWriteToFileCodes(const std::string file, const std::vector<size_t> &codes)
+{
+    std::vector<byte> bytes{};
+    ProtoConvertCodesToBytes(codes, bytes);
+    std::ofstream out;
+    out.open(file, std::ios::out | std::ios::binary); // or use full path "C:/example.bin"
+    out.write(reinterpret_cast<const char *>(& bytes[0]), bytes.size());
+    out.close();
+}
 int main() {
-    std::vector<size_t> codes{ 0b110001010, 0b101010101, 0b100001111, 0b1, 0b1};
+    std::vector<size_t> codes{ 0b110001010, 0b101010101, 0b100001111, 0b1, 0b1, 0b1111111111};
     std::vector<size_t> codes2;
     std::vector<byte> bytes;
-
-    PrototypeConvertCodesToBytes(codes, bytes);
+    
+    ProtoConvertCodesToBytes(codes, bytes);
 
     for (size_t i = 0; i < bytes.size(); i++) {
-        std::cout << std::bitset<8>(bytes[i]) << std::endl;
+        std::cout << std::bitset<8>(bytes[i]) << "\t" << (int)bytes[i] << std::endl;
     }
+    ProtoConverBytesToCodes(bytes, codes2);
 
-    PrototypeConverBytesToCodes(bytes, codes2);
+    for (size_t i = 0; i < codes2.size(); i++)
+    {
+        std::cout << std::bitset<CODE_SIZE>(codes2[i]) << std::endl;
+    }
+    codes2.resize(0);
 
+    ProtoWriteToFileCodes("./test.bin", codes);
+    ProtoReadFromFileConvertToCodes("./test.bin", codes2);
+    std::cout << "After read\n";
     for (size_t i = 0; i < codes2.size(); i++)
     {
         std::cout << std::bitset<10>(codes2[i]) << std::endl;
     }
-
     return 0;
 }
